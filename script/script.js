e22c2baebdf787b77d3023275127867950bdb025
minhajul islam <mm01882390860@gmail.com>
2026-02-19 17:58:51 +0600

    for loop practce from marhaba book

---
+++ script/script.js
@@ -0,0 +1,44 @@
+const numbers =[12, 98, 45, 63, 21, 72];
+
+for (const num of numbers){
+    console.log(num);
+}
+
+const fruits = ["orange", "apple", "banana", "jackfruit", "watermelon"];
+
+for (const item of fruits){
+    console.log(item);
+}
+
+
+const subjacts = ["Arabic", "Urdu", "Farsi", "Hindi", "Afgani"];
+
+for (const s of subjacts){
+    console.log(s);
+}
+
+const Manus = ["Chiken tanduri", "Beef kacchi", "lacchi", "Shami kabab", "Morog Polaw"];
+
+for (const manu of Manus){
+    console.log(manu);
+}
+
+
+const dateOfBirth =[1975, 1980, 2003, 2007, 2019, 2022];
+
+
+for (const date of dateOfBirth){
+    console.log(date);
+}
+
+const players = ["Abdul malek", "Abdul matin", "Abu sayeed", "abu taher misbah", "Muhammadullah hafijji"];
+
+for (const alim of players){
+    console.log(alim);
+}
+
+const ExamDate = [3, 16, 20, 25, 28];
+
+for (const ex of ExamDate){
+    console.log(ex);
+}
