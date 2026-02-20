483406b4f114da46c10da3ef8acfd1b20aa3425e
minhajul islam <mm01882390860@gmail.com>
2026-02-20 23:26:00 +0600

    tamplate string practice

---
+++ script/tamplateString.js
@@ -0,0 +1,39 @@
+/*
+*tamplate string
+*es6 feature
+*eti backticks (`) bebohar kore lekha hoy
+*string ebong variable ek sathe lekha jay
+*multiline string lekha jay
+*${} er modhye variable ba expression lekha jay
+*/
+
+
+// tamplate string by backticks (`)
+
+
+let name = ``;
+
+// string ebong variable ek sathe lekha jay
+
+let firstName = 'minhajul';
+let lastName = 'islam';
+
+let fullName = `my name is ${firstName} ${lastName}`;
+
+console.log(fullName);
+
+// multiline string lekha jay
+
+let multilineString = `this is a multiline string
+this is the second line
+this is the third line`;
+
+console.log(multilineString);
+
+// ${} er modhye variable ba expression lekha jay
+let a = 5;
+let b = 10;
+
+let result = `the sum of ${a} and ${b} is ${a + b}`;
+
+console.log(result);
