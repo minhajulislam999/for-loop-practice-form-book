76a982cbf6341d328a5a207b27d9bf40bc18940d
minhajul islam <mm01882390860@gmail.com>
2026-02-21 00:11:19 +0600

    single line arrow function

--- script/arrowFunction.js
+++ script/arrowFunction.js
@@ -13,11 +13,19 @@ function add(num1, num2){
 console.log(add(5, 10));
 
 
-// function expression
+// 2 function expression
 
 const addition = function(num1, num2){
     return num1 + num2;
 }
 
 console.log(addition(3, 8));
+
+
+// 3 Arrow function =>
+
+    const add2 = (num1, num2) => num1 + num2;
+
+    console.log(add2(4,6));
+
 
