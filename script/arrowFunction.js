45c67362007ced4800324a1fdd4fcc61cd291038
minhajul islam <mm01882390860@gmail.com>
2026-02-21 00:07:47 +0600

    practice: normal function and function expression

---
+++ script/arrowFunction.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+// Normal function declaration
+
+function add(num1, num2){
+    let a = num1 + num2;
+    return a;
+    
+}
+
+console.log(add(5, 10));
+
+
+// function expression
+
+const addition = function(num1, num2){
+    return num1 + num2;
+}
+
+console.log(addition(3, 8));
+
