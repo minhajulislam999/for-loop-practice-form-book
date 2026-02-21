10d8ce3f1e6551631e279ede1cea9ee0dd66e77a
minhajul islam <mm01882390860@gmail.com>
2026-02-21 12:52:57 +0600

    Object.values(), and .entries() practice

---
+++ script/objectMethods.js
@@ -0,0 +1,31 @@
+//keys
+
+//object.keys() object er sob property name (key) array akare dey
+
+const employee = {
+    name: 'Raja Rani',
+    designation: 'Qa',
+    salary: 50000,
+    experience: 1
+
+}
+
+console.log(Object.keys(employee));
+ // ['name', 'designation', 'salary', 'experience']
+
+
+ //Object.values() object er sob value array akare dey
+
+ const values = Object.values(employee);
+
+ console.log(values);
+    // ['Raja Rani', 'Qa', 50000, 1]
+
+
+    //object.entries() object er sob property name (key) and value array akare dey array of array akare dey
+
+    const entries = Object.entries(employee);
+
+    console.log(entries);
+    // [['name', 'Raja Rani'], ['designation', 'Qa'], ['salary', 50000], ['experience', 1]]
+    
