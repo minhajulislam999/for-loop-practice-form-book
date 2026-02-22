ab49051d345188d13fbd2201867dfa42c49a9472
minhajul islam <mm01882390860@gmail.com>
2026-02-22 23:55:27 +0600

    object literal added

--- script/object.js
+++ script/object.js
@@ -7,4 +7,15 @@
 //functions are stored as key : function() pairs called methods.
 
 
+// Object example
 
+const car = {
+    type: "Fiat",
+    model: "500",
+    color: "white",
+}
+
+//Object Literal
+
+{firstName : "John",lastName :"Doe",age:50, eyeColor: "blue"}
+//output: {firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue"}
