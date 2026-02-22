744e944d24625bd8ed8348f8eac7070e2cabd7fd
minhajul islam <mm01882390860@gmail.com>
2026-02-22 23:48:30 +0600

    object deffination added

---
+++ script/object.js
@@ -0,0 +1,10 @@
+// what are javascript objects?
+
+//objects are variables that can store both values and functions.
+
+//values are stored as key : value pairs called properties.
+
+//functions are stored as key : function() pairs called methods.
+
+
+
