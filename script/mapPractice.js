be48c22c5ed3e7aed9c166f4c3d1dd8b586277f3
minhajul islam <mm01882390860@gmail.com>
2026-02-24 19:20:46 +0600

    map practice

---
+++ script/mapPractice.js
@@ -0,0 +1,20 @@
+// const nums = [3,6,2,8,9];
+// nums.map((n =>{
+//    return  result = n * n;
+//     console.log(result);
+// }))
+
+
+
+const nums = [3,6,8,9];
+
+const squard = nums.map((n)=> n * n);
+
+console.log(squard);
+
+
+const names = ["Ab", "Ks", "LpL", "Jsx"]
+
+const uperCase = names.map((name)=> name.toLocaleUpperCase());
+
+console.log(uperCase);
