4c3823ad3f84d2c5915e008af6267b0306e735e0
minhajul islam <mm01882390860@gmail.com>
2026-02-24 20:12:45 +0600

    more practice map and filter

--- script/optionalChaining.js
+++ script/optionalChaining.js
@@ -7,5 +7,22 @@ const user ={
     }
 }
 
-console.log(user?.unexpected);
+// console.log(user?.unexpected);
 
+
+const products = [
+  {name: "Phone", price: 10000},
+  {name: "Laptop", price: 50000},
+  {name: "Watch", price: 2000}
+];
+
+const price = products.filter((pro)=>pro.price > 5000)
+console.log(price);
+//output: [] decouse ekhane 5000 tk upore kono product nai
+
+const names = products.map((name)=>{
+    return name.name;
+})
+
+// console.log(names);
+//[ 'Phone', 'Laptop', 'Watch' ] this is output 
